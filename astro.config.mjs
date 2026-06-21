b06397ef720d0e5dbfaf3f38b6871563c85f036a
Zain <zainul_a@hotmail.co.uk>
2026-06-21 20:21:26 +0100

    hi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
+  site: 'https://yoghurtofyouth.co.uk',
+  integrations: [
+    react(),
+    sitemap({
+      filter: (page) => !page.includes('/success'),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
